74d283bbd5db8e7d8bf00647d55b4025fcc4ba6a
Jorge Segura <solutionjweb@gmail.com>
2023-08-08 09:53:48 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jorge Segura on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
